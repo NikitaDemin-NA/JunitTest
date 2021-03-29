069c35afcf3886a09a5aeda58c0365ebc23124be
Никита <>
2021-03-30 00:32:02 +0300

    добавил шаг Yandex авторизации

--- JunitTest/src/test/java/TestSelenium.java
+++ JunitTest/src/test/java/TestSelenium.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.interactions.Actions;
 
 import java.security.Key;
 import java.time.Duration;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 
@@ -18,17 +19,22 @@ public class TestSelenium {
     System.setProperty("webdriver.chrome.driver", "/Users/andrei/testrepository2/JunitTest/src/main/resources/drivers/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://passport.yandex.ru/auth?origin=home_yandexid&retpath=https%3A%2F%2Fyandex.ru&backpath=https%3A%2F%2Fyandex.ru/");
-//        driver.get("https://yandex.ru/");
-//        driver.findElement(By.xpath("//a[@href='https://passport.yandex.ru/auth?origin=home_desktop_ru&retpath=https%3A%2F%2Fmail.yandex.ru%2F&backpath=https%3A%2F%2Fyandex.ru']")).click();
+        driver.get("https://yandex.ru/");
+        driver.findElement(By.xpath("//a[@href='https://passport.yandex.ru/auth?origin=home_desktop_ru&retpath=https%3A%2F%2Fmail.yandex.ru%2F&backpath=https%3A%2F%2Fyandex.ru']")).click();
         Thread.sleep(2000);
-        Actions action = new Actions(driver);
-//        WebElement element = driver.findElement(By.xpath("//a[@href='https://yandex.ru/support/passport/']"));
-//        WebElement element = driver.findElement(By.xpath("//span[@class='Textinput Textinput_focused Textinput_hovered Textinput_view_floating-label Textinput_size_l']"));
+        String window1 = driver.getWindowHandle();
+        Set<String> currentWindows = driver.getWindowHandles();
+        String window2 = null;
+        for (String window : currentWindows){
+            if (!window.equals(window1)){
+                window2=window;
+                break;
+            }
+        };
+        driver.switchTo().window(window2);
+
         WebElement element = driver.findElement(By.xpath("//*[@id='passp-field-login']"));
         element.click();
-//        action.moveToElement(element).build().perform();
         element.sendKeys("asdcfg98@ya.ru", Keys.ENTER);
-
     }
 }
